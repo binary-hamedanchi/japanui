3585f84f385cfc38e50189b932ea1a728baccb74
binary-maksym <maksym@binary.com>
2016-07-11 14:00:50 +0800

    updates

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -35,6 +35,9 @@ module.exports = {
     filename: 'bundle.js',
   },
   plugins: [
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.UglifyJsPlugin(),
+    new webpack.optimize.AggressiveMergingPlugin(),
     new webpack.DefinePlugin({
       'process.env': {
         NODE_ENV: JSON.stringify('production'),
