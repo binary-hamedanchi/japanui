7c3c59800063efa62662c2bb32f8af27c74d21a7
binary-maksym <maksym@binary.com>
2016-07-13 12:04:03 +0800

    styling

--- src/js/components/JapanPriceButton.jsx
+++ src/js/components/JapanPriceButton.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const JapanPriceButton = ({ price, cb, dynamics }) => (
-  <button onClick={cb} className='price-button price-wrapper'>
+  <button onClick={cb} className='price-button'>
     {price}<span className='dynamics'>{dynamics}</span>
   </button>
 );
