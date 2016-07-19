d1fb248b283b5da18dd5bc7f433829e9b996861c
binary-maksym <maksym@binary.com>
2016-07-19 13:21:44 +0800

    updates

--- src/js/patches/socketConfig.js
+++ src/js/patches/socketConfig.js
@@ -1,8 +1,15 @@
 import socket from 'config/socket.json';
 
 const socketConfig = () => {
-  if (typeof window.getSocketURL === 'function') {
-    socket.url = window.getSocketURL();
+  if (typeof window.getSocketURL === 'function' &&
+    typeof window.getAppId === 'function' &&
+    typeof window.page === 'object' &&
+    typeof window.page.language === 'function') {
+    const url = window.getSocketURL();
+    const appId = window.getAppId();
+    const language = window.page.language() ? `&l=${window.page.language()}` : '';
+
+    socket.url = `${url}?app_id=${appId}${language}`;
   }
 
   return socket;
