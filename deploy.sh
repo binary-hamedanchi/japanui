8b375f6cca3c0f728d3a84e9b53911c44baf5820
binary-maksym <maksym@binary.com>
2016-09-06 20:53:32 +0800

    travis fix

--- deploy.sh
+++ deploy.sh
@@ -10,8 +10,9 @@ function doCompile {
 
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
 if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
-    echo "Skipping deploy; just doing a build."
-    doCompile
+    # echo "Skipping deploy; just doing a build."
+    echo "Skipping deploy;"
+    # doCompile
     exit 0
 fi
 
