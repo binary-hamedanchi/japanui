c35a5c57b8345d51fc301f77c977b69d73610b0f
binary-hamedanchi <hamedanchi@gmail.com>
2016-09-23 10:16:19 +0800

    Change tooltip to text

--- src/js/components/PayoutSelectContainer.jsx
+++ src/js/components/PayoutSelectContainer.jsx
@@ -9,11 +9,13 @@ const PayoutSelectContainer = (props) => {
     .reduce((props2, key) => (props2[key] = props[key], props2), {});
 
   return (<InputBlock
-    heading={text('Payout Amount')}
-    tooltip={text('min 1,000 and max 100,000')}>
+    heading={text('Payout Amount')}>
       <div className='payout-input'>
-      ¥ <input {...props2} id='payout'/>
-        <label className='payout-mult' htmlFor='payout'>,000</label>
+        <div className='float-left'>
+          ¥ <input {...props2} id='payout'/>
+          <label className='payout-mult' htmlFor='payout'>,000</label>
+        </div>
+        <div className='float-left hint'>{text('min: 1,000')}<br />{text('max: 100,000')}</div>
       </div>
     </InputBlock>);
 }
